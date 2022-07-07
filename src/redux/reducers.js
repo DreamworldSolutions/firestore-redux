b4ac2c14d9a33e5f0317401148fe16a1b91133e0
“Nirmal <nirmalb@dreamworld.dev>
2022-07-07 15:55:20 +0530

    fix(reducers): fix typo.

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -164,6 +164,7 @@ const firestoreReducer = (state = INITIAL_STATE, action) => {
       if (!action.options.localWrite) {
         return state;
       }
+      const docs = action.docs;
       forEach(docs, (doc) => {
         const pathSegments = action.collectionPath.split("/");
         const collection = pathSegments[pathSegments.length - 1];
